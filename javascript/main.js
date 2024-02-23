2b72d94ffc07c65c06a61aff45af4306b5b96ed4
ViRaS360 <73831108+ViRaS360@users.noreply.github.com>
2024-02-23 12:22:12 +0530

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,17 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links")
 
 const menuBtnIcon = menuBtn.querySelector("i");
+// const observer = new IntersectionObserver((entries)=>{
+//   entries.forEach((entry)=>{
+//     console.log(entry)
+//     if(entry.isIntersecting){
+//       entry.target.classList.add('.show');
+//     }
+//   });
+// });
+// const hiddenElements = document.querySelectorAll(".hidden");
+
+// hiddenElements.forEach((el)=>observer.observe(el));
 
 menuBtn.addEventListener("click",(e)=>{
     navLinks.classList.toggle("open");
@@ -13,3 +24,29 @@ navLinks.addEventListener("click",(e)=>{
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class","ri-menu-line");
 });
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
